31e2563c02bdd3a698938c951362ee76b46b184e
101113608 <48575130+101113608@users.noreply.github.com>
2025-05-09 02:32:32 +1000

    Add sidebar-project-list module
    To generate a DOM element that will contain a list of the user's current
    projects onto the page (specifically the sidebar), the
    'sidebar-projects-list' JS module was added.

---
+++ src/js/interface/sidebar/sidebar-projects-list.js
@@ -0,0 +1,50 @@
+function createSidebarProjectsList(projectsList) {
+    const ul = document.createElement("ul");
+
+    for (let [index, project] of projectsList.entries()) {
+        const li = createProjectListItem(project, index);
+        ul.append(li);
+    }
+
+    return ul;
+}
+
+function createProjectListItem(project, index) {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+
+    if (index === 0) {
+        button.classList.add("selected-project");
+    }
+
+    button.setAttribute("data-project-index", index);
+    button.classList.add("project-list-item");
+    button.append(createProjectTitle(project.getTitle()), createTodoCount(project.getTodoList().length));
+
+    li.append(button);
+
+    return li;
+}
+
+function createProjectTitle(projectTitle) {
+    const span = document.createElement("span");
+
+    span.setAttribute("data-project-title", "");
+    span.classList.add("project-title");
+    span.textContent = projectTitle;
+
+    return span;
+}
+
+function createTodoCount(todoLength) {
+    const span = document.createElement("span");
+
+    span.setAttribute("data-todo-count", "");
+    span.classList.add("todo-count");
+    span.textContent = todoLength;
+
+    return span;
+}
+
+
+export { createSidebarProjectsList };
