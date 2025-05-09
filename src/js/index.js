7173b1240f0c96e730a81a5588f9df0d975a4eef
101113608 <48575130+101113608@users.noreply.github.com>
2025-05-10 01:47:46 +1000

    Highlight project on sidebar click
    To emphasise what project the user is currently on, index.js was updated
    to change the colour of a project list item upon clicking one of them.

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,12 @@ import { createSidebarProjectsList } from "./interface/sidebar/sidebar-projects-
 
     // Data/Objects
     const projects = new Projects();
+    const projectTracker = {
+        index: 0,
+        getProject: () => {
+            return projects.getProjectViaIndex(projectTracker.index)
+        },
+    }
 
     function renderSidebarMain(projectsList) {
         if (projectsList) {
@@ -21,5 +27,42 @@ import { createSidebarProjectsList } from "./interface/sidebar/sidebar-projects-
         }
     }
 
+    function highlightProjectElement(sidebarElement, projectIndex) {
+        const ul = Array
+            .from(sidebarElement.firstElementChild.childNodes)
+            .filter(element => element.nodeName.toLowerCase() === "li");
+
+        const selectedProject = sidebarElement.querySelector(`[data-project-index="${projectIndex}"]`);
+
+        for (let li of ul) {
+            const button = li.querySelector("[data-project-index]");
+            button.classList.remove("selected-project");
+        }
+
+        if (selectedProject) {
+            selectedProject.classList.add("selected-project");
+        } else {
+            console.error(`Unable to retrieve selected project of index ${projectIndex}.`)
+        }
+    }
+
+    sidebar.addEventListener("click", e => {
+        if (e.target.hasAttribute("data-add-project-btn")) {
+            openModal("Add Project", createProjectModal("Add"));
+            return;
+        }
+
+        if (e.target.closest("[data-project-index]")) {
+            projectTracker.index = e.target.closest("[data-project-index]").getAttribute("data-project-index");
+            highlightProjectElement(sidebarProjectsList, projectTracker.index);
+
+            return;
+        }
+    });
+
+    projects.addProject(createProject("Hello World"));
+
     renderSidebarMain(projects.getProjectsList());
+    highlightProjectElement(sidebarProjectsList, projectTracker.index);
+
 })();
