a72c5e17c21c282ddbe40948be3992e6361ff657
101113608 <48575130+101113608@users.noreply.github.com>
2025-05-10 01:30:36 +1000

    Update index.js to display projects list, btn icon
    To automatically display what projects (todo lists) the user currently
    has, index.js has been updated to perform said functionality.
    
    As the projects list is now generated via index.js, the temporary html
    that was used to style the look of the projects was removed.
    
    Additionally, the HTML of the 'New Project' button was updated with a
    new icon, as the 'Add new icons, illustrations' commit removed the old
    icon that the button was using previously.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 import "../styles.css";
+import { Projects, createProject, createTodo } from "./logic/projects";
+import { createSidebarProjectsList } from "./interface/sidebar/sidebar-projects-list";
+
+(function() {
+    // DOM elements
+    const body = document.querySelector("body");
+    const main = document.querySelector("main");
+    const sidebar = document.querySelector("aside");
+    const sidebarProjectsList = document.querySelector("[data-projects-list]");
+
+    // Data/Objects
+    const projects = new Projects();
+
+    function renderSidebarMain(projectsList) {
+        if (projectsList) {
+            const projectsListElement = createSidebarProjectsList(projectsList);
+
+            sidebarProjectsList.textContent = "";
+            sidebarProjectsList.append(projectsListElement);
+        }
+    }
+
+    renderSidebarMain(projects.getProjectsList());
+})();
